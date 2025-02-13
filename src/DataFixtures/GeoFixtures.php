76ad452269142fe6e7f9711459ecae6d8a604a4c
Sir Septimus <125373660+AuroreMOMYM22004066@users.noreply.github.com>
2025-02-13 09:56:44 +0100

    init database

---
+++ src/DataFixtures/GeoFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Region;
+use App\Entity\Departement;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GeoFixtures extends Fixture
+{
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Récupérer les régions
+        $response = $this->httpClient->request('GET', 'https://geo.api.gouv.fr/regions');
+        $regionsData = $response->toArray();
+
+        $regions = [];
+        foreach ($regionsData as $data) {
+            $region = new Region();
+            $region->setLibelle($data['nom']);
+            $manager->persist($region);
+            $regions[$data['code']] = $region; // Stocker pour lier aux départements
+        }
+        $manager->flush();
+
+        // Récupérer les départements
+        $response = $this->httpClient->request('GET', 'https://geo.api.gouv.fr/departements');
+        $departementsData = $response->toArray();
+
+        foreach ($departementsData as $data) {
+            $departement = new Departement();
+            $departement->setLibelle($data['nom']);
+            $departement->setCode($data['code']);
+
+            // Lier au bon objet Region
+            if (isset($regions[$data['codeRegion']])) {
+                $departement->setRegion($regions[$data['codeRegion']]);
+            }
+
+            $manager->persist($departement);
+        }
+
+        $manager->flush();
+    }
+}
