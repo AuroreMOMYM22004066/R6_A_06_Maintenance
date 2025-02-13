542e7fb70e2d244407058b41a1c24510fbb78389
Roubaud-Axel-2225073b <axel.roubaud@etu.univ-amu.fr>
2025-02-13 10:31:28 +0100

    feat : linter test

--- src/DataFixtures/GeoFixtures.php
+++ src/DataFixtures/GeoFixtures.php
@@ -2,8 +2,8 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Region;
 use App\Entity\Departement;
+use App\Entity\Region;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
